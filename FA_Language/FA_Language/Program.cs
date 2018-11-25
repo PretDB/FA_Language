40a950284b77fa80678d49781730d98d7ee7b781
Pret D.B <pret-db@qq.com>
2018-11-25 14:50:50 +0800

    Add C# project file and basic classes (not completed).

---
+++ FA_Language/FA_Language/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FA_Language/FA_Language_Syntax/Syntaxer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace FA
+{
+    public enum Type
+    {
+        None,
+        Int,
+        Real,
+        Imag,
+        Function,
+        String,
+        List,
+        Tuple
+    }
+
+    static public class Syntaxer
+    {
+    }
+
+    public class SyntaxNode
+    {
+
+    }
+
+    public class Function
+    {
+        public string name;
+
+        private FA.Type returnType;
+        private List<FA.Type> paramList;
+
+        public Function(string name, FA.Type retType, List<FA.Type> paramList)
+        {
+            this.name = name;
+            this.returnType = retType;
+            this.paramList = paramList;
+        }
+
+        public FA.Type Run()
+        {
+
+            return FA.Type.None;
+        }
+
+    }
+}
